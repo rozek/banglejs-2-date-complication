47e1fac37beb4f94008eb918f31713aa2a25294d
Andreas Rozek <A.Rozek@gmx.de>
2022-01-15 17:53:29 +0100

    Update Demo.js

--- Demo.js
+++ Demo.js
@@ -1,7 +1,7 @@
   let Clockwork = require('https://raw.githubusercontent.com/rozek/banglejs-2-simple-clockwork/main/Clockwork.js');
 
   Clockwork.windUp({
-    face:  require('https://raw.githubusercontent.com/rozek/banglejs-2-twelve-fold-face/main/ClockFace.js'),
+    face:  require('https://raw.githubusercontent.com/rozek/banglejs-2-twelve-numbered-face/main/ClockFace.js'),
     hands: require('https://raw.githubusercontent.com/rozek/banglejs-2-rounded-clock-hands/main/ClockHands.js'),
     complications: {
       b:require('https://raw.githubusercontent.com/rozek/banglejs-2-date-complication/main/Complication.js'),
